07bcc7447504c973c191dccc0b63dc10d9567fbe
susanodii <odiisusan@gmail.com>
2024-10-02 19:24:57 +0100

    second card

--- src/dashbord/components/Cards.js
+++ src/dashbord/components/Cards.js
@@ -3,8 +3,8 @@ import React from "react";
 
 const Cards = () => {
   return (
-    <div className='flex flex-col  justify-start mt-[3rem] border border-black'>
-      <section className='flex justify-around border border-red'>
+    <div className='flex flex-col  justify-start mt-[3rem] '>
+      <section className='flex justify-around '>
         <div className='w-[510px] h-[330px] bg-[#D9D9D9] rounded-3xl flex flex-col items-center justify-center font-poppins'>
           <p className='text-[36px] font-bold leading-[54px] font-poppins '>
             Athens
@@ -32,7 +32,7 @@ const Cards = () => {
               </p>
             </div>
             {/* second */}
-            <div className='flex flex-col gap-4'>
+            <div className='flex flex-col gap-4 '>
               <div className='flex gap-4'>
                 {" "}
                 <img src='assets/sunrise-white -1.png' />
@@ -62,10 +62,61 @@ const Cards = () => {
             </div>
           </div>
           {/* second row in the card */}
-          <div className=''>
+          <div className=' flex flex-col justify-around items-center'>
             <img src='assets/clear 1.png' alt='sun' />
+            <p className='text-[32px] font-extrabold leading-[48px] font-poppins'>
+              {" "}
+              Sunny
+            </p>
           </div>
-          <div></div>
+
+          <div className='flex gap-[2rem]  py-2'>
+            <div className=' h-full  flex flex-col gap-4 '>
+              <div className='flex flex-col  '>
+                <img src='assets/humidity-img.png' alt='sun' width={70} />
+                <p className='text-[20px] font-semibold leading-[30px] font-poppins mt-3'>
+                  41%
+                </p>
+                <p className='text-[16px] font-medium leading-[24px] font-poppins mt-2'>
+                  Humidity
+                </p>
+              </div>
+
+              <div className='flex flex-col   '>
+                <img src='assets/pressure-white 1.png' alt='sun' width={70} />
+                <p className='text-[20px] font-semibold leading-[30px] font-poppins'>
+                  197hpa
+                </p>
+                <p className='text-[16px] font-medium leading-[24px] font-poppins mt-2'>
+                  Humidity
+                </p>
+              </div>
+            </div>
+
+            {/*  */}
+            <div className='h-full  flex flex-col gap-4'>
+              <div className='flex flex-col   '>
+                <img src='assets/wind 1.png' alt='sun' width={70} />
+                <p className='text-[20px] font-semibold leading-[30px] font-poppins'>
+                  2km/h
+                </p>
+                <p className='text-[16px] font-medium leading-[24px] font-poppins mt-2'>
+                  Wind Speed
+                </p>
+              </div>
+
+              <div className='flex flex-col items-center  '>
+                <img src='assets/uv-white 1.png' alt='sun' width={70} />
+                <p className='text-[20px] font-semibold leading-[30px] font-poppins'>
+                  8
+                </p>
+                <p className='text-[16px] font-medium leading-[24px] font-poppins mt-2'>
+                  UV
+                </p>
+              </div>
+              {/* <div></div> */}
+            </div>
+          </div>
         </div>
       </section>
 
